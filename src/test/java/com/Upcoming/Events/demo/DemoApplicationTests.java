57e1189beedab8b435b106f3576791f88a579794
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-02-14 14:14:49 +0100

    first commit

---
+++ src/test/java/com/Upcoming/Events/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Upcoming.Events.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
