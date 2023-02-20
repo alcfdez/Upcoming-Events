ee36fb89e698f62af03318d3ffc21452b314786e
alcfdez <alcfdez@hotmail.com>
2023-02-20 15:33:58 +0100

    init routers

--- src/main/frontend/src/layouts/MainLayout.vue
+++ src/main/frontend/src/layouts/MainLayout.vue
@@ -1,8 +1,26 @@
+<script>
+import EssentialLinkVue from 'src/components/EssentialLink.vue';
 
-<template>
+const linksList= [
+  {
+    title:'Dashboard',
+    caption:'dasboard',
+    link:'dashboard'
+  },
+
+]
+
+</script>
+
+
+
+
+<!-- <template>
   <q-layout view="hHh lpR fFf">
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
+ -->
+
---
+++ src/main/frontend/src/pages/Dashboard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SliderComponent from 'src/components/SliderComponent.vue';
+import HeadPage from  '../components/HeadPage.vue'
+</script>
+
+<template>
+  <q-page class="bg-blue column items-center">
+    <HeadPage/>
+    <SliderComponent/>
+
+    
+<q-footer class="footer bg-blue flex flex-center" reveal>
+    <img class="logo" src="../assets/images/logo.png" />
+</q-footer>
+  </q-page>
+
+</template>
+
