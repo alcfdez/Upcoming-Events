6e49a579a27e14d9ec9b8a1a1497594988c75f1a
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-02-27 14:19:08 +0100

    add user model

--- src/main/java/com/Upcoming/Events/demo/models/User.java
+++ src/main/java/com/Upcoming/Events/demo/models/User.java
@@ -1,6 +1,5 @@
 package com.Upcoming.Events.demo.models;
 
-import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +11,57 @@ import javax.persistence.Table;
 @Table(name = "users")
 
 public class User {
-    @Id 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @Column (name = "id_user")
+    
+    private long id;    
+    @Column (name = "username")
+    private String username;
+    @Column (name = "password")
+    private String password;
+    @Column (name = "role")
+    private String role;
+
+     public User () {
+     }
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    
+    
+
+     
 }
