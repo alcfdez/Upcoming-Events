bb9b80d36a78510112b1bdab617f776699976356
JehiselRuth <jehisel.rrp@gmail.com>
2023-03-01 11:47:10 +0100

    Unauthorized post

--- src/main/java/com/Upcoming/Events/demo/controllers/EventController.java
+++ src/main/java/com/Upcoming/Events/demo/controllers/EventController.java
@@ -5,14 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.Upcoming.Events.demo.models.Event;
 import com.Upcoming.Events.demo.services.EventService;
 
@@ -24,7 +17,7 @@ public class EventController {
     private EventService eventService;
     
 //Crear nuevo usuario
-    @PostMapping 
+    @PostMapping(value ="", consumes = "application/*") 
     public ResponseEntity<?> create(@RequestBody Event event) {
         return ResponseEntity.status(HttpStatus.CREATED).body(eventService.save(event));
  }
