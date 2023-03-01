2e48e22d9041cbe1e56e9c952cc240e8914631f3
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-03-01 10:42:22 +0100

    index finish

--- src/main/frontend/src/pages/IndexPage.vue
+++ src/main/frontend/src/pages/IndexPage.vue
@@ -3,12 +3,13 @@
 import { defineComponent } from 'vue'
 import SliderComponent from 'src/components/SliderComponent.vue';
 import ConcertList from 'src/components/ConcertList.vue';
-
+import HeadPage from 'src/components/HeadPage.vue';
 </script>
 
 
 <template>
   <q-page class="bg-blue column items-center">
+    <HeadPage />
     <SliderComponent />
     <ConcertList />
 
