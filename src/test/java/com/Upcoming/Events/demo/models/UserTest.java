be785d255ff48ee990f58dec93d3648930992e96
Enol-Igareta <enol1code@gmail.com>
2023-03-06 12:33:34 +0100

    test entity user

---
+++ src/test/java/com/Upcoming/Events/demo/models/UserTest.java
@@ -0,0 +1,33 @@
+package com.Upcoming.Events.demo.models;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class UserTest {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    public void testCountryPersistedInDatabase() {
+        User paco = new User("Paco", "pass", null);
+
+        entityManager.persist(paco);
+        entityManager.flush();
+
+        User retrievedCountry = entityManager.find(User.class, paco.getId());
+
+        assertThat(retrievedCountry).isNotNull();
+        assertThat(retrievedCountry.getUsername()).isEqualTo("Paco");
+    }
+}
